a0d56bcfd2016d135d1af919c367b71f4aa23983
Dmitry <dmitry.fedorovv@gmail.com>
2025-03-25 23:47:49 +0300

    UPD recipes page

--- webpack.common.js
+++ webpack.common.js
@@ -134,6 +134,171 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/recipes/kulebyaka-s-kapustoy.html',
       filename: './recipes/kulebyaka-s-kapustoy.html'
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/recipes/rzhanie-lepeshki-na-skovorode.html',
+      filename: './recipes/rzhanie-lepeshki-na-skovorode.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/pirog-s-brusnikoy.html',
+      filename: './recipes/pirog-s-brusnikoy.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/vatrushki-s-tvorogom.html',
+      filename: './recipes/vatrushki-s-tvorogom.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/shangi-s-kartofelem.html',
+      filename: './recipes/shangi-s-kartofelem.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/kulebyaka-s-ryboy-i-risom.html',
+      filename: './recipes/kulebyaka-s-ryboy-i-risom.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/sladkie-pirozhki-s-morkovyu-i-izyumom.html',
+      filename: './recipes/sladkie-pirozhki-s-morkovyu-i-izyumom.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/chak-chak-domashniy.html',
+      filename: './recipes/chak-chak-domashniy.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/postnye-lepeshki-na-kartofelnom-otvare.html',
+      filename: './recipes/postnye-lepeshki-na-kartofelnom-otvare.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/belyashi-s-myasom-po-chuvashski.html',
+      filename: './recipes/belyashi-s-myasom-po-chuvashski.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/khachapuri-po-imeretinski.html',
+      filename: './recipes/khachapuri-po-imeretinski.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/chechenskie-hingaly-s-myasom.html',
+      filename: './recipes/chechenskie-hingaly-s-myasom.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/gata-armyanskaya.html',
+      filename: './recipes/gata-armyanskaya.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/lepeshki-na-matsoni-s-zelenyu.html',
+      filename: './recipes/lepeshki-na-matsoni-s-zelenyu.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/zyryanskiye-pirozhki-s-repoy.html',
+      filename: './recipes/zyryanskiye-pirozhki-s-repoy.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/tonkie-rzhanie-lepeshki-shangi.html',
+      filename: './recipes/tonkie-rzhanie-lepeshki-shangi.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/pirog-s-pshenkoy-i-tvorogom.html',
+      filename: './recipes/pirog-s-pshenkoy-i-tvorogom.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/medovye-shangi-s-klyukvoy.html',
+      filename: './recipes/medovye-shangi-s-klyukvoy.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/pachat.html',
+      filename: './recipes/pachat.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/pshennyy-pirog-s-tykvoy.html',
+      filename: './recipes/pshennyy-pirog-s-tykvoy.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/shanezhki-s-tvorogom.html',
+      filename: './recipes/shanezhki-s-tvorogom.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/kapustnyy-pirog-na-chugunke.html',
+      filename: './recipes/kapustnyy-pirog-na-chugunke.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/rasstegai-s-ryboy.html',
+      filename: './recipes/rasstegai-s-ryboy.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/peremyachi-s-myasom.html',
+      filename: './recipes/peremyachi-s-myasom.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/pirog-s-kapustoy-i-gribami.html',
+      filename: './recipes/pirog-s-kapustoy-i-gribami.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/sladkie-vatrushki-s-varenyem.html',
+      filename: './recipes/sladkie-vatrushki-s-varenyem.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/kulochki-s-povidlom.html',
+      filename: './recipes/kulochki-s-povidlom.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/pirog-s-lukom-i-yaytsom.html',
+      filename: './recipes/pirog-s-lukom-i-yaytsom.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/sloyenye-yazychki-s-saharom.html',
+      filename: './recipes/sloyenye-yazychki-s-saharom.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/plyushki-moskovskie.html',
+      filename: './recipes/plyushki-moskovskie.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/pirozhki-s-gorbushey-i-risom.html',
+      filename: './recipes/pirozhki-s-gorbushey-i-risom.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/belyashi-s-morskoy-kapustoy-i-yaytsom.html',
+      filename: './recipes/belyashi-s-morskoy-kapustoy-i-yaytsom.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/presnye-lepeshki-s-lukom-i-solyu.html',
+      filename: './recipes/presnye-lepeshki-s-lukom-i-solyu.html'
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: './src/recipes/pirog-s-cheremuhoy-i-smetannoy-zalivkoy.html',
+      filename: './recipes/pirog-s-cheremuhoy-i-smetannoy-zalivkoy.html'
     })
 
     // Article
